7dc72ff70c59bb0c3faf3196b8fb997f86979314
ANKIT-KANDULNA <144378696+ANKIT-KANDULNA@users.noreply.github.com>
2025-04-23 17:54:25 +0530

    Update db.js

--- server/db/db.js
+++ server/db/db.js
@@ -4,7 +4,7 @@ const pool=new Pool({
   user:'postgres',       // my postgres username
   host:'localhost',      // where PostgreSQL is running
   database:'restaurant_management_system',  // my database  name
-  password:'kandulna@920',   // my postgres password
+  password:'your_pass',   // my postgres password
   port:5432,             // default postgres port
 });
 
